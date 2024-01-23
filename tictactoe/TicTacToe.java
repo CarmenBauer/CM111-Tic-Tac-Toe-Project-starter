4f38e778f17b81bb355839f4c6139cda63a71bb4
CarmenBauer <156852094+CarmenBauer@users.noreply.github.com>
2024-01-23 14:01:12 -0600

    Checkpoint 1 Complete

--- tictactoe/TicTacToe.java
+++ tictactoe/TicTacToe.java
@@ -1,5 +1,9 @@
 public class TicTacToe {
-    public static void main(String[] args) {
+   public static void main( String [] args )
+    {
+        String spot = ".|.|."; // spot where you can choose "X" or "O"
+        String board = "-----"; // can't put your "X" or "O" on the board that would be ridiculous
 
+        System.out.printf( " %s%n %s%n %s%n %s%n %s%n", spot, board, spot, board, spot );
     }
 }
